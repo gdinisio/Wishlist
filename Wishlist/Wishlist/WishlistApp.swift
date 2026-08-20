23177f271529da4e831525f292e0fd6134c14c88
gdinisio <gdinisio@icloud.com>
2026-08-20 13:33:18 +0200

    Initial Commit

---
+++ Wishlist/Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Giovanni Di Nisio on 20/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
